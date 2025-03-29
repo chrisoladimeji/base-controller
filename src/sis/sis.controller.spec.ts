023bd4fa810f28d14f4985a10b99fa9a29b77a93
joan_the_narc <ryanbrogley@gmail.com>
2025-03-29 14:53:06 -0400

    DigiCred branding and modified scripts

--- src/sis/sis.controller.spec.ts
+++ src/sis/sis.controller.spec.ts
@@ -3,19 +3,47 @@ import { SisLoaderService } from "./loaders/sisLoader.service";
 import { SisController } from "./sis.controller";
 import { SisService } from "./sis.service";
 import { StudentsService } from "./students/students.service";
-import { mock } from "node:test";
+import { ModuleMocker } from "jest-mock"
+import { Test } from "@nestjs/testing";
+import { of } from "rxjs";
 
+const testStudentId = {
+    studentName: 'Michael Jordan',
+    studentNumber: '0023'
+}
 
 describe('SisController', () => {
 
-    let studentsService: StudentsService;
-    let loaderService: SisLoaderService;
-    let configService: ConfigService;
     let sisController: SisController;
     let sisService: SisService;
 
-    beforeEach(() => {
+    beforeEach(async () => {
+        const moduleRef = await Test.createTestingModule({
+            controllers: [SisController],
+            providers: [
+                {
+                    provide: SisService,
+                    useValue: {
+                        getStudentId: jest.fn(() => of(testStudentId)),
+                    }
+                }
+            ],
+        }).compile()
 
+        sisController = moduleRef.get(SisController);
+    })
+
+    it('should be defined', () => {
+        expect(sisController).toBeDefined();
+    });
+
+
+
+    describe('getStudentId', () => {
+        it('should return a studentid when given a number', async () => {
+            sisController.getStudentId('0023').then(response =>
+            expect(response).toEqual(testStudentId));
+        })
     })
 
 
