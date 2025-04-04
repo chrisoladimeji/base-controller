b6024bca83cd159321a003937ff08946743d56ec
brogleyr <ryanbrogley@gmail.com>
2025-04-04 00:01:04 -0400

    Added StudentId

--- src/dtos/transcript.dto.ts
+++ src/dtos/transcript.dto.ts
@@ -5,11 +5,13 @@ export class TranscriptDto {
     // The full transcript if they can't send labeled data, may be a PDF
     studentTranscript: string;
 
+    studentId: string;
     studentFullName: string;
-    studentSsn: string;
+
     studentBirthDate: string;
     studentAddress: string;
     studentPhone: string;
+    studentSsn: string;
     
     emergencyContactName: string;
     emergencyContactPhone: string;
