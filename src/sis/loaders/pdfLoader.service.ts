77dcf024af8f2069c4839ce454435acb74443762
brogleyr <ryanbrogley@gmail.com>
2025-04-14 09:52:37 -0400

    parse student, school, and fields that appear once

--- src/sis/loaders/pdfLoader.service.ts
+++ src/sis/loaders/pdfLoader.service.ts
@@ -7,7 +7,6 @@ import { HighSchoolTranscriptDto, TranscriptDto } from "../../dtos/transcript.dt
 import { ConfigService } from "@nestjs/config";
 import * as Zip from 'adm-zip';
 import * as Pdf from 'pdf-parse'
-import { RedisService } from "../../services/redis.service";
 
 @Injectable()
 export class PdfLoaderService extends SisLoaderService {
@@ -63,10 +62,39 @@ export class PdfLoaderService extends SisLoaderService {
         const pdfText = pdfParser.text.split(/(\n| {3})/)
             .map(str => String(str).trim())
             .filter(str => str);
-        
-        const studentNumberBlock = pdfText.filter(str => str.startsWith("Student Number:"))[0];
+
+        console.log(pdfText);
+
         // "Student Number: ########"
-        studentId.studentNumber = studentNumberBlock.match(/\d+/)[0];
+        studentId.studentNumber = pdfText.filter(str => str.startsWith("Student Number:"))[0]?.match(/\d+/)[0];
+        studentId.studentFullName = pdfText[8];
+        studentId.studentBirthDate = pdfText.filter(str => str.startsWith("Birthdate:"))[0].match(/\d+\/\d+\/\d+/)[0];
+        studentId.studentPhone = pdfText.filter(str => str.startsWith("Tel:"))[1]?.split(": ")[1];
+        studentId.gradeLevel = pdfText.filter(str => str.startsWith("Grade:"))[0]?.split(": ")[1];
+        studentId.graduationDate = pdfText[pdfText.indexOf("Graduation Year:") + 1];
+        studentId.schoolName = pdfText[1].substring(0, pdfText[1].indexOf(" Official Transcript"));
+        studentId.schoolPhone = pdfText.filter(str => str.startsWith("Tel:"))[0]?.split(": ")[1];
+
+        transcript.transcriptDate = pdfText.filter(str => str.startsWith("Generated on"))[0]?.match(/\d+\/\d+\/\d+/)[0];
+        // transcript.transcriptComments
+        transcript.studentNumber = studentId.studentNumber;
+        transcript.studentFullName = studentId.studentFullName;
+        transcript.studentBirthDate = studentId.studentBirthDate;
+        transcript.studentPhone = studentId.studentPhone;
+        transcript.studentAddress = pdfText[12];
+        transcript.gradeLevel = studentId.gradeLevel;
+        transcript.graduationDate = studentId.graduationDate;
+        transcript.schoolName = studentId.schoolName;
+        transcript.schoolPhone = studentId.schoolPhone;
+        transcript.schoolAddress = pdfText[7];
+        transcript.schoolFax = pdfText.filter(str => str.startsWith("Fax:"))[0]?.split(": ")[1];
+        transcript.schoolCode = pdfText.filter(str => str.startsWith("School Code:"))[0]?.split(": ")[1];
+        transcript.gpa = parseFloat(pdfText.filter(str => str.startsWith("Cumulative GPA"))[0].match(/\d\.\d+/)[0]);
+        // transcript.earnedCredits
+        transcript.gpaUnweighted = parseFloat(pdfText.filter(str => str.startsWith("Cumulative GPA"))[1].match(/\d\.\d+/)[0]);
+
+        console.log(studentId);
+        console.log(transcript);
 
         return [studentId, transcript];
     }    
