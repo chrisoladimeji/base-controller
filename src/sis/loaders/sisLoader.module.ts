b520a84a0a2082e093e90c59602e8517ee2c1097
brogleyr <ryanbrogley@gmail.com>
2025-04-20 01:30:27 -0400

    Moved loader selection to environment variable

--- src/sis/loaders/sisLoader.module.ts
+++ src/sis/loaders/sisLoader.module.ts
@@ -1,30 +1,65 @@
-import { Module } from '@nestjs/common';
-import { SisLoaderService } from "./sisLoader.service";
-import { TestLoaderService } from "./testLoader.service";
+import { DynamicModule, Module, Provider, Type } from '@nestjs/common';
+import { SisLoaderService } from './sisLoader.service';
+import { TestLoaderService } from './testLoader.service';
 import { PdfLoaderService } from './pdfLoader.service';
 import { RedisService } from '../../services/redis.service';
 import { CsvLoaderService } from './csvLoader.service';
 import { NhcsLoaderService } from './nhcsLoader.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { EllucianService } from 'src/ellucian/ellucian.service';
+import { EllucianModule } from 'src/ellucian/ellucian.module';
+import { HttpModule } from '@nestjs/axios';
 
-const sisLoaderService = {
-    //TODO Get service from env
-
-    provide: SisLoaderService,
-    //useClass: TestLoaderService
-    // useClass: PdfLoaderService
-    // useClass: CsvLoaderService
-    useClass: NhcsLoaderService
-};
-
-@Module({
-    providers: [
-        sisLoaderService,
-        RedisService,
-        CsvLoaderService,
-        PdfLoaderService,
-    ],
-    exports: [SisLoaderService]
-})
+@Module({})
 export class SisLoaderModule {
-
+    static registerAsync(): DynamicModule {
+        return {
+            module: SisLoaderModule,
+            imports: [
+                ConfigModule,
+                EllucianModule,
+                HttpModule,
+            ],
+            providers: [
+                RedisService,
+                CsvLoaderService,
+                PdfLoaderService,
+                TestLoaderService,
+                NhcsLoaderService,
+                EllucianService,
+                {
+                    provide: SisLoaderService,
+                    inject: [
+                        ConfigService,
+                        TestLoaderService,
+                        NhcsLoaderService,
+                        PdfLoaderService,
+                        EllucianService,
+                    ],
+                    useFactory: (
+                        configService: ConfigService,
+                        test: TestLoaderService,
+                        nhcs: NhcsLoaderService,
+                        pdf: PdfLoaderService,
+                        ellucian: EllucianService,
+                    ): SisLoaderService => {
+                        const type = configService.get<string>('LOAD_TYPE')?.toUpperCase();
+                        switch (type) {
+                            case 'TEST':
+                                return test;
+                            case 'NHCS':
+                                return nhcs;
+                            case 'PENDER':
+                                return pdf;
+                            case 'CFCC':
+                                return ellucian;
+                            default:
+                                throw new Error(`Unknown LOAD_TYPE: ${type}`);
+                        }
+                    },
+                }
+            ],
+            exports: [SisLoaderService],
+        };
+    }
 }
--- src/sis/sis.module.ts
+++ src/sis/sis.module.ts
@@ -1,21 +1,13 @@
 import { Module } from '@nestjs/common';
 import { SisService } from './sis.service';
 import { SisController } from './sis.controller';
-import { StudentsModule } from './students/students.module';
 import { SisLoaderModule } from './loaders/sisLoader.module';
-import { EnrollmentsModule } from './enrollments/enrollments.module';
-import { SchoolsModule } from './schools/schools.module';
-import { CoursesModule } from './courses/courses.module';
-import { SessionsModule } from './sessions/sessions.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
-    CoursesModule,
-    EnrollmentsModule,
-    SchoolsModule,
-    SessionsModule,
-    StudentsModule,
-    SisLoaderModule
+    ConfigModule,
+    SisLoaderModule.registerAsync(),
   ],
   providers: [
     SisService,
