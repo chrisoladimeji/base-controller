31e610b46ac5eff0f975060d69c37cb0dfe47990
chrisoladimeji <chris@verid.id>
2025-06-16 10:09:32 -0400

    build: Fix CI workflow and update Jest config

--- jest.config.js
+++ jest.config.js
@@ -1,17 +1,34 @@
 // jest.config.js
 module.exports = {
-  moduleFileExtensions: ["js", "json", "ts"],
-  rootDir: "src", // Look for tests in the 'src' directory
-  testRegex: ".*\\.spec\\.ts$", // Find all files ending in .spec.ts
-  transform: {
-    "^.+\\.(t|j)s$": "ts-jest",
+  // Use ts-jest preset to get sensible defaults for TypeScript projects
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+
+  // The root of your source code, typically the project root
+  rootDir: '.',
+
+  // A list of paths to directories that Jest should use to search for files in
+  roots: ['<rootDir>/src'],
+
+  // The file patterns Jest uses to detect test files
+  testRegex: '.*\\.spec\\.ts$',
+
+  // A map from regular expressions to module names or to arrays of module names
+  // that allow to stub out resources with a single module.
+  // THIS IS THE FIX for "Cannot find module 'src/...'" errors.
+  moduleNameMapper: {
+    '^src/(.*)$': '<rootDir>/src/$1',
   },
-  collectCoverageFrom: ["**/*.(t|j)s"],
-  coverageDirectory: "../coverage",
-  testEnvironment: "node",
-  // Add our custom Slack reporter
+
+  // Where to output coverage reports
+  coverageDirectory: './coverage',
+
+  // An array of glob patterns indicating a set of files for which coverage information should be collected
+  collectCoverageFrom: ['src/**/*.(t|j)s'],
+
+  // Your custom Slack reporter configuration
   reporters: [
-    "default",
-    "<rootDir>/../slack-reporter.js" // Path from 'src' to the root
+    'default',
+    '<rootDir>/slack-reporter.js' // Path from the project root
   ],
 };
